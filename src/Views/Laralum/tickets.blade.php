a696105c20fdf52f0f66a3be5c0981bc02c5fb00
24aitor <1aitorribaarchilla@gmail.com>
2017-02-16 21:25:17 +0100

    Unused view removed

--- src/Views/Laralum/tickets.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('laralum::layouts.master')
-@section('icon', 'mdi-ticket')
-@section('title', trans('laralum_tickets::tickets.tickets'))
-@section('subtitle', trans('laralum_tickets::tickets.tickets_desc'))
-@section('content')
-
-@endsection
--- src/Views/Public/tickets.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('laralum::layouts.public')
-@section('title', trans('laralum_tickets::tickets.tickets'))
-@section('content')
-
-@endsection

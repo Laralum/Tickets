baaa65eb0aea2d56a63f203c4652139e86c9063b
24aitor <1aitorribaarchilla@gmail.com>
2017-04-09 12:50:25 +0200

    Typo

--- src/TicketsServiceProvider.php
+++ src/TicketsServiceProvider.php
@@ -126,7 +126,7 @@ class TicketsServiceProvider extends ServiceProvider
         }
 
         $this->publishes([
-            __DIR__.'/Views/Public' => resource_path('views/vendor/Laralum/Tickets'),
+            __DIR__.'/Views/public' => resource_path('views/vendor/Laralum/Tickets'),
         ], 'laralum_tickets');
 
         $this->app->register('GrahamCampbell\\Markdown\\MarkdownServiceProvider');
